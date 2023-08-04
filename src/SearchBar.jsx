e1b88b7a58957772f0e62295c83472b317881728
yingcyl <chiayinglow@gmail.com>
2023-08-04 17:06:46 +0100

    Create searchbar component

--- src/SearchBar.jsx
+++ src/SearchBar.jsx
@@ -1,3 +1,20 @@
-export default function SearchBar() {
-  return <div></div>;
+import { useState } from "react";
+
+export default function SearchBar({ formSubmit }) {
+  const [text, setText] = useState("");
+  const handleChange = (e) => {
+    setText(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    formSubmit(text);
+  };
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input type="text" onChange={handleChange} value={text} />
+      </form>
+    </div>
+  );
 }
