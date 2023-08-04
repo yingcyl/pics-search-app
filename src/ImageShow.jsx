80b973ab5e52b4a75874060c2c4187b99a36ac72
yingcyl <chiayinglow@gmail.com>
2023-08-04 17:07:22 +0100

    Create imageshow component

--- src/ImageShow.jsx
+++ src/ImageShow.jsx
@@ -1,3 +1,7 @@
-export default function ImageShow() {
-  return <div></div>;
+export default function ImageShow({ image }) {
+  return (
+    <div>
+      <img src={image.urls.small} alt={image.alt_description} />
+    </div>
+  );
 }
